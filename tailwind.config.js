c100ca4be139a38822648187348773b658cc7749
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-28 20:56:15 +0600

    feat: setup radix colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+function getColorScale(name) {
+  let scale = {};
+  for (let i = 1; i <= 12; i++) {
+    scale[i] = `var(--${name}-${i})`;
+    // next line only needed if using alpha values
+    scale[`a${i}`] = `var(--${name}-a${i})`;
+  }
+
+  return scale;
+}
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: getColorScale("indigo"),
+        gray: getColorScale("slate"),
+      },
+    },
   },
   plugins: [],
-}
+};
