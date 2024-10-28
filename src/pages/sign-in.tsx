5ca7e6300534ceeb8d24a9a8f9be0233a7262d7e
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-28 23:11:52 +0600

    feat: sign up page design done

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -5,29 +5,31 @@ import { Link } from "react-router-dom";
 const SignIn = () => {
   return (
     <section className="h-[calc(100vh-56px)] w-full max-w-screen-lg mx-auto px-4 flex items-center justify-center">
-      <Card className="w-full max-w-sm mx-auto space-y-3" size="3">
-        <Heading as="h3">Sign In</Heading>
-        <TextField.Root placeholder="Enter Your Email" type="text">
-          <TextField.Slot>
-            <RiMailLine className="size-4 text-gray-10" />
-          </TextField.Slot>
-        </TextField.Root>
-        <TextField.Root placeholder="Enter Your Password" type="password">
-          <TextField.Slot>
-            <RiLockPasswordLine className="size-4 text-gray-10" />
-          </TextField.Slot>
-        </TextField.Root>
-        <div className="w-full flex items-center justify-end">
-          <Button>Sign In</Button>
-        </div>
-        <p className="text-sm font-normal text-gray-9">
-          Don't have any account?{" "}
-          <Link to="/signup" className="text-accent-9">
-            Sign Up
-          </Link>{" "}
-          Now.
-        </p>
-      </Card>
+      <form className="w-full max-w-xs mx-auto">
+        <Card className="space-y-3" size="3">
+          <Heading as="h3">Sign In</Heading>
+          <TextField.Root placeholder="Enter Your Email" type="email">
+            <TextField.Slot>
+              <RiMailLine className="size-4 text-gray-10" />
+            </TextField.Slot>
+          </TextField.Root>
+          <TextField.Root placeholder="Enter Your Password" type="password">
+            <TextField.Slot>
+              <RiLockPasswordLine className="size-4 text-gray-10" />
+            </TextField.Slot>
+          </TextField.Root>
+          <div className="w-full flex items-center justify-end">
+            <Button className="hover:cursor-pointer">Sign In</Button>
+          </div>
+          <p className="text-sm font-normal text-gray-9">
+            Don't have any account?{" "}
+            <Link to="/signup" className="text-accent-9">
+              Sign Up
+            </Link>{" "}
+            Now.
+          </p>
+        </Card>
+      </form>
     </section>
   );
 };
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,7 +1,41 @@
+import { Button, Card, Heading, TextArea, TextField } from "@radix-ui/themes";
+import { RiLockPasswordLine, RiMailLine, RiUser3Line } from "@remixicon/react";
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
   return (
-    <section className="py-14 -mt-14 h-[calc(100vh-56px)] w-full max-w-screen-lg mx-auto px-4 flex items-center justify-center">
-      <h1>This is Sign Up page</h1>
+    <section className="h-[calc(100vh-56px)] w-full max-w-screen-lg mx-auto px-4 flex items-center justify-center">
+      <form className="w-full max-w-xs mx-auto">
+        <Card className="space-y-3" size="3">
+          <Heading as="h3">Sign Up</Heading>
+          <TextField.Root placeholder="Enter Your Name" type="text">
+            <TextField.Slot>
+              <RiUser3Line className="size-4 text-gray-10" />
+            </TextField.Slot>
+          </TextField.Root>
+          <TextField.Root placeholder="Enter Your Email" type="email">
+            <TextField.Slot>
+              <RiMailLine className="size-4 text-gray-10" />
+            </TextField.Slot>
+          </TextField.Root>
+          <TextField.Root placeholder="Enter Your Password" type="password">
+            <TextField.Slot>
+              <RiLockPasswordLine className="size-4 text-gray-10" />
+            </TextField.Slot>
+          </TextField.Root>
+          <TextArea placeholder="Enter Your Bio" />
+          <div className="w-full flex items-center justify-end">
+            <Button type="submit" className="hover:cursor-pointer">Sign Up</Button>
+          </div>
+          <p className="text-sm font-normal text-gray-9">
+            Already have an account?{" "}
+            <Link to="/signin" className="text-accent-9">
+              Sign In
+            </Link>{" "}
+            Now.
+          </p>
+        </Card>
+      </form>
     </section>
   );
 };
