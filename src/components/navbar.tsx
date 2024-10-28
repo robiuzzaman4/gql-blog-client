2da00564de647d0938624e0c98513f2dcf8618d2
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-28 21:14:34 +0600

    fix: fix font issue

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@radix-ui/themes";
 import { RiBloggerFill } from "@remixicon/react";
 import { Link } from "react-router-dom";
 
@@ -6,9 +7,14 @@ const Navbar = () => {
     <nav className="fixed top-0 w-full bg-white border-b h-14">
       <div className="h-full w-full max-w-screen-lg mx-auto px-4 flex items-center justify-between">
         <Link to="/" className="flex items-center gap-1">
-          <RiBloggerFill className="text-indigo-600"/>
+          <RiBloggerFill className="text-indigo-600" />
           <p className="text-base font-semibold">GQL Blogs</p>
         </Link>
+        <div className="flex items-center gap-2">
+          <Link to="/signin">
+            <Button className="hover:cursor-pointer">Sign In</Button>
+          </Link>
+        </div>
       </div>
     </nav>
   );
