cefbefb3fcd62ff2c2aade081ce583d7798ec390
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-29 11:54:05 +0600

    feat: fetch blog posts

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,21 @@ import { Theme } from "@radix-ui/themes";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="light">
-      <Theme>
-        <RouterProvider router={router} />
-      </Theme>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider defaultTheme="light">
+        <Theme>
+          <RouterProvider router={router} />
+        </Theme>
+      </ThemeProvider>
+    </ApolloProvider>
   </StrictMode>
 );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,44 @@
+import { gql, useQuery } from "@apollo/client";
+
+const POSTS = gql`
+  query posts {
+    posts {
+      id
+      title
+      content
+      published
+      createdAt
+      author {
+        id
+        name
+        email
+      }
+    }
+  }
+`;
+
 const HomePage = () => {
+  const { loading, error, data } = useQuery(POSTS);
+
+  const posts = data.posts;
+  console.log("posts", posts);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error?.message}</p>;
+
   return (
     <section className="py-14 w-full max-w-screen-lg mx-auto px-4">
-      <h1>This is home page</h1>
+      {posts && posts?.length > 0 ? (
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {posts?.map((post) => (
+            <div key={post?.id}>{post?.title}</div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No posts found.</p>
+        </div>
+      )}
     </section>
   );
 };
