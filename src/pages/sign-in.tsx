f10d2bdcd90a1849be677351f71531111444a755
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-29 21:47:33 +0600

    feat: signin mutation done

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,25 +1,89 @@
-import { Button, Card, Heading, TextField } from "@radix-ui/themes";
+import { gql, useMutation } from "@apollo/client";
+import { Button, Card, Heading, Spinner, TextField } from "@radix-ui/themes";
 import { RiLockPasswordLine, RiMailLine } from "@remixicon/react";
-import { Link } from "react-router-dom";
+import { FormEvent } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+const SIGNIN = gql`
+  mutation signin($email: String!, $password: String!) {
+    signin(email: $email, password: $password) {
+      token
+      message
+    }
+  }
+`;
 
 const SignIn = () => {
+  // sign in mutations state and hooks
+  const [signin, { loading }] = useMutation(SIGNIN);
+
+  // router
+  const navigate = useNavigate();
+
+  // handle sign in
+  const handleSignIn = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+
+    const payload = {
+      email: form.email.value,
+      password: form.password.value,
+    };
+
+    try {
+      const response = await signin({
+        variables: {
+          ...payload,
+        },
+      });
+      const message = response?.data?.signin?.message;
+      if (message === "Signin Successfull!") {
+        toast.success(message);
+        navigate("/");
+      } else {
+        toast.error(message);
+      }
+    } catch (error) {
+      console.log("signin error: ", error);
+    } finally {
+      form.reset();
+    }
+  };
+
   return (
     <section className="h-[calc(100vh-56px)] w-full max-w-screen-lg mx-auto px-4 flex items-center justify-center">
-      <form className="w-full max-w-xs mx-auto">
+      <form onSubmit={handleSignIn} className="w-full max-w-xs mx-auto">
         <Card className="space-y-3" size="3">
           <Heading as="h3">Sign In</Heading>
-          <TextField.Root placeholder="Enter Your Email" type="email">
+          <TextField.Root
+            placeholder="Enter Your Email"
+            type="email"
+            name="email"
+          >
             <TextField.Slot>
               <RiMailLine className="size-4 text-gray-10" />
             </TextField.Slot>
           </TextField.Root>
-          <TextField.Root placeholder="Enter Your Password" type="password">
+          <TextField.Root
+            placeholder="Enter Your Password"
+            type="password"
+            name="password"
+          >
             <TextField.Slot>
               <RiLockPasswordLine className="size-4 text-gray-10" />
             </TextField.Slot>
           </TextField.Root>
           <div className="w-full flex items-center justify-end">
-            <Button className="hover:cursor-pointer">Sign In</Button>
+            <Button
+              type="submit"
+              className="hover:cursor-pointer"
+              disabled={loading}
+            >
+              <Spinner loading={loading} />
+              Sign In
+            </Button>
           </div>
           <p className="text-sm font-normal text-gray-9">
             Don't have any account?{" "}
